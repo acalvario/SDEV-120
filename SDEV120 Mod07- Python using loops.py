9aa3131b020065c370df61b61563fbbc5b4ecbb1
acalvario <acalvario@ivytech.edu>
2025-10-07 14:58:21 -0400

    Update SDEV120 Mod07- Python using loops.py

--- SDEV120 Mod07- Python using loops.py
+++ SDEV120 Mod07- Python using loops.py
@@ -1,6 +1,6 @@
 # Program to check if numbers in a list are even or odd
-# Author: [Your Name]
-# Date: [Today's Date]
+# Author: [Ana Calvario]
+# Date: [October 07, 2025]
 #List of 15 numbers
 numbers = [2, 5, 6, 10, 13, 15, 18, 20, 1, 11, 4, 30, 50, 40, 60]
 # Loop through the list and check even or odd
@@ -9,3 +9,4 @@ for number in numbers:
         print(str(number) + " is even")
     else:
         print(str(number) + " is odd")
+
